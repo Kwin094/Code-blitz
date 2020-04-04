03cdb669e2329bc5354f77cd0195a820fe03c411
Kwin094 <62923495+Kwin094@users.noreply.github.com>
2020-04-04 14:56:49 -0700

    Update signup.view.ts

--- signup.view.ts
+++ signup.view.ts
@@ -16,23 +16,23 @@ export class SignupView {
         this.app = document.getElementById('root')
             const html=`
             <div class = 'box'>
-                <img src="http://icon-library.com/images/blitz-icon/blitz-icon-18.jpg" alt="Blitz Icon" width="128" height="128">
+                <img src="https://lh5.googleusercontent.com/e1zlxn6kj-9k2QuNR1aDVkmNYXoDwdvXMF_k-6L-LmbB4Ny_RdH1GnAD9fBMbW8RhBikZz5c=w16383" alt="Blitz Icon" width="215" height="134">                
                 <form id='Signup' method='post'>
-                     <h1>Clode Blitz Signup</h1>
+                     <h1>Signup</h1>
                 <div>
                     <img src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/user-512.png" alt="user Icon" width="20" height="20">
-                    <input name='name' type='text' placeholder='Name' />            
-                    </div>
+                    <input name='name' type='text' placeholder='Name' required="" />            
+                </div>
                 <div>
                      <img src="http://icons.iconarchive.com/icons/custom-icon-design/mono-general-2/512/mail-icon.png" alt="Mail Icon" width="20" height="20">
-                    <input name='email' type='text' placeholder='Email' />
+                    <input name='email' type='text' placeholder='Email' required=""/>
                  </div>
                 <div>
                     <img src="https://cdn2.iconfinder.com/data/icons/flat-ui-icons-24-px/24/lock-24-512.png" alt="Lock Icon" width="20" height="20">
-                    <input name='password' type='password' placeholder='Password' />
+                    <input name='password' type='password' placeholder='Password' required="" />
                 </div>
                 <div>
-                    <button type='submit'>Let's Code!</button>
+                    <button type='submit' >Let's Code!</button>
                 </div>
             </div>
         `;
